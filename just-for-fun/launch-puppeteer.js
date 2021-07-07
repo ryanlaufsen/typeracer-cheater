596e254a129bd9970cd6aad3b2aa84bd4394b28b
Ryan <Ryan>
2021-07-06 18:32:00 -0700

    dumb delete

--- just-for-fun/launch-puppeteer.js
+++
@@ -1,9 +0,0 @@
-const puppeteer = require('puppeteer');
-
-(async () => {
-    const browser = await puppeteer.launch({
-        headless: false,
-        args: ['--remote-debugging-port=42069'],
-        width: 1000, height: 1000
-    });
-})()
